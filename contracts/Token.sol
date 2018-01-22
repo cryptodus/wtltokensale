b999818168278f842754762cb3816b5e5539bd78
vbcca <>
2018-01-22 20:04:12 +0200

    add basic token info

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,5 +3,7 @@ pragma solidity ^0.4.18;
 import 'zeppelin-solidity/contracts/token/MintableToken.sol';
 
 contract Token is MintableToken {
-
+  string public constant name = "TOKEN";
+  string public constant symbol = "TOK";
+  uint8 public constant decimals = 18;
 }
