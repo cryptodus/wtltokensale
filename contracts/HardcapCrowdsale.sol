b4a9ffced900210b7dacab7f5ec4c03d777fd335
vbcca <vybccal@gmail.com>
2018-03-06 23:00:07 +0200

    Add ico phases and their change mechanism Add posibility to assign tokens without investment

--- contracts/HardcapCrowdsale.sol
+++ contracts/HardcapCrowdsale.sol
@@ -1,33 +1,33 @@
 pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
-import 'zeppelin-solidity/contracts/lifecycle/Pausable.sol';
+import "zeppelin-solidity/contracts/crowdsale/distribution/FinalizableCrowdsale.sol";
 import './HardcapToken.sol';
 
-/*
-  Taxi crowdsale is Pausable contract it is paused on init
-  and may be paused any time in the process. While it is paused
-  it can finalized meaning all left tokens will be assigned to owner wallet
-*/
-contract HardcapCrowdsale is MintedCrowdsale, Pausable {
+contract HardcapCrowdsale is MintedCrowdsale, FinalizableCrowdsale {
   using SafeMath for uint256;
 
-  uint256 private constant TOKENS_RATE_CHANGE_STEP = 65 * 10**24;
-  uint256 private constant INIT_RATE = 11500;
-  uint256 private constant MIN_RATE = 10000;
-  uint256 private constant RATE_STEP = 500;
+  struct Phase {
+    uint from;
+    uint to;
+    uint256 cap;
+    uint256 rate;
+  }
+
+  uint256 private constant TEAM_PERCENTAGE = 10;
+  uint256 private constant PLATFORM_PERCENTAGE = 25;
+  uint256 private constant CROWDSALE_PERCENTAGE = 65;
 
   uint256 private leftovers = 65 * 10**24;
-  uint256 private toSellTillNextStep = TOKENS_RATE_CHANGE_STEP;
-
-  bool public isFinalized = false;
+  uint256 private currentCap;
 
   address public overflowOwner;
   uint256 public overflowAmount;
 
   address public platform;
+  uint256 public phase = 1;
 
-  event Finalized();
+  mapping (uint => Phase) private phases;
 
   modifier notFinished() {
     require(leftovers > 0);
@@ -36,20 +36,44 @@ contract HardcapCrowdsale is MintedCrowdsale, Pausable {
   }
 
   function HardcapCrowdsale(address _wallet, address _platform, HardcapToken _token) public
-    Crowdsale(INIT_RATE, _wallet, _token) {
-      paused = true;
+    Crowdsale(1340, _wallet, _token)
+    TimedCrowdsale(1522072800, 1528984800) {
       platform = _platform;
+      currentCap = 10 * 10**24;
+      phases[1] = Phase(1522072800, 1522936800, 10 * 10**24, 1340);
+      phases[2] = Phase(1522936800, 1523800800, 7 * 10**24, 1290);
+      phases[3] = Phase(1523800800, 1524664800, 7 * 10**24, 1240);
+      phases[4] = Phase(1524664800, 1525528800, 7 * 10**24, 1190);
+      phases[5] = Phase(1525528800, 1526392800, 7 * 10**24, 1140);
+      phases[6] = Phase(1526392800, 1527256800, 9 * 10**24, 1090);
+      phases[7] = Phase(1527256800, 1528120800, 9 * 10**24, 1050);
+      phases[8] = Phase(1528120800, 1528984800, 9 * 10**24, 1000);
   }
 
-  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) notFinished whenNotPaused internal {
+  /*
+    If investmend was made in bitcoins etc. owner can assign apropriate amount of
+    tokens to the investor.
+  */
+  function assignTokens(address _beneficiary, uint256 weiAmount) onlyOwner public {
+    _preValidatePurchase(_beneficiary, weiAmount);
+    uint256 tokens = _getTokenAmount(weiAmount);
+    _processPurchase(_beneficiary, tokens);
+    _updatePurchasingState(_beneficiary, weiAmount);
+    _postValidatePurchase(_beneficiary, weiAmount);
+  }
+
+  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) notFinished internal {
     super._preValidatePurchase(_beneficiary, _weiAmount);
-    require(_weiAmount > 0);
+
+    if (phases[phase].to < getBlockTimestamp()) {
+      _changePhase();
+    }
   }
 
   function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
     uint256 _tokens = _weiAmount.mul(rate);
-    if (toSellTillNextStep > _tokens && leftovers > _tokens) {
-      toSellTillNextStep = toSellTillNextStep.sub(_tokens);
+    if (currentCap > _tokens && leftovers > _tokens) {
+      currentCap = currentCap.sub(_tokens);
       leftovers = leftovers.sub(_tokens);
       return _tokens;
     }
@@ -60,18 +84,18 @@ contract HardcapCrowdsale is MintedCrowdsale, Pausable {
     while (leftovers > 0 && _weiAmount > 0) {
       uint256 _stepTokens = 0;
 
-      if (toSellTillNextStep < _tokens) {
-          _stepTokens = toSellTillNextStep;
-          toSellTillNextStep = TOKENS_RATE_CHANGE_STEP;
+      if (currentCap < _tokens) {
+          _stepTokens = currentCap;
+          currentCap = 0;
           _weiReq = _stepTokens.div(rate);
           _weiAmount = _weiAmount.sub(_weiReq);
-          _calcNextRate();
+          _changePhase();
       } else {
           _stepTokens = leftovers;
           if (leftovers > _tokens) {
             _stepTokens = _tokens;
           }
-          toSellTillNextStep = toSellTillNextStep.sub(_stepTokens);
+          currentCap = currentCap.sub(_stepTokens);
           _weiReq = _stepTokens.div(rate);
           _weiAmount = _weiAmount.sub(_weiReq);
       }
@@ -99,11 +123,11 @@ contract HardcapCrowdsale is MintedCrowdsale, Pausable {
     }
   }
 
-  function _calcNextRate() internal {
-      rate = rate.sub(RATE_STEP);
-      if (rate < MIN_RATE) {
-        rate = MIN_RATE;
-      }
+  function _changePhase() {
+    require(phase < 8);
+    phase = phase.add(1);
+    currentCap = currentCap.add(phases[phase].cap);
+    rate = phases[phase].rate;
   }
 
   /*
@@ -117,33 +141,21 @@ contract HardcapCrowdsale is MintedCrowdsale, Pausable {
       overflowAmount = _weiAmount;
   }
 
-  /*
-    This is ripped of from zeppelin contracts, because zeppelin FinalzableCrowdsale
-    contract extends TimedCrowdsale contract and here it is not the case
-  */
-  function finalize() onlyOwner whenPaused public {
-    require(!isFinalized);
-
-    finalization();
-    Finalized();
-
-    isFinalized = true;
-  }
-
   function finalization() internal {
     HardcapToken token = HardcapToken(token);
+
     // mint and burn all leftovers
-    require(token.mint(wallet, leftovers));
-    token.burn(leftovers);
+    uint256 tokenCap = token.totalSupply().mul(100).div(CROWDSALE_PERCENTAGE);
 
-    // mint all the dedicated tokens to wallet
-    require(token.mint(wallet, 10 * 10**24));
-
-    // mint all the dedicated tokens to platfrom
-    require(token.mint(wallet, 25 * 10**24));
-
+    require(token.mint(wallet, tokenCap.mul(TEAM_PERCENTAGE).div(100)));
+    require(token.mint(platform, tokenCap.mul(PLATFORM_PERCENTAGE).div(100)));
+    token.burn(token.cap().sub(token.totalSupply()));
 
     require(token.finishMinting());
     token.transferOwnership(wallet);
+  }
+
+  function getBlockTimestamp() internal constant returns (uint256) {
+    return block.timestamp;
   }
 }
